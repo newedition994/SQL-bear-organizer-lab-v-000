f317af0948f4c0fbef1228edbb449822e4fa7b02
Breyanna Vass <breyanna.bvass@gmail.com>
2017-11-16 17:20:46 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Mr.Chocolate", 20, "M", "dark brown", "calm", 0)
 
-INSERT INTO bears(name, age, gender, color, temperament, alive)
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Rowdy", 10, "M", "black", "intense", 1)
 
 INSERT INTO bears(name, age, gender, color, temperament, alive)
 
