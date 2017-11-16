98bdd1a30d83dada8fe1247b65b024e8e054076f
Breyanna Vass <breyanna.bvass@gmail.com>
2017-11-16 17:15:58 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO bears(name, age, gender, color, temperament, alive)
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES()
 
 INSERT INTO bears(name, age, gender, color, temperament, alive)
 
