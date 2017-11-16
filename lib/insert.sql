4413fe821cdb03cade9dd2bae08ed180ed2216bb
Breyanna Vass <breyanna.bvass@gmail.com>
2017-11-16 17:27:26 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 20, "M", "dark brown", "calm", 0)
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 10, "M", "black", "intense", 1)
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "F", "dark brown", "Nice", 1)
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sargeant Brown", 19, "M", "Green", "Slimy", 0)
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 13, "F", "dark brown", "goofy", 1)
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "M", "Black", "Grinchy", 1)
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "F", "Blue", "naive", 1)
-
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 20, "M", "black", "aggressive", 0)
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Mr. Chocolate', 4, 'M', 'white', 'morose', 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Rowdy', 1, 'M', 'red', 'rowdy', 0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Tabitha', 6, 'F', 'black', 'happy', 0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Sergeant Brown', 11, 'F', 'beige', 'haughty', 1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Melissa', 1, 'F', 'brown', 'calm', 0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Grinch', 4, 'M', 'green','grumpy',1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Wendy', 5, 'F', 'white','sassy',1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES (NULL, 3, 'M', 'brown','murderous',0);
