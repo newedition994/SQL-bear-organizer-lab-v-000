1f7fe397276bae5e54a3e61d4bb7165d9f6ef86a
Breyanna Vass <breyanna.bvass@gmail.com>
2017-11-16 17:28:03 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,7 +2,7 @@ INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Mr. Chocola
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Rowdy', 1, 'M', 'red', 'rowdy', 0);
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Tabitha', 6, 'F', 'black', 'happy', 0);
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Sergeant Brown', 11, 'F', 'beige', 'haughty', 1);
-INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Melissa', 1, 'F', 'brown', 'calm', 0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Melissa', 1, 'F', 'brown', 'relaxed', 0);
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Grinch', 4, 'M', 'green','grumpy',1);
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Wendy', 5, 'F', 'white','flirty',1);
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES (NULL, 3, 'M', 'brown','aggressive',0);
